38baf48508bbb796244acff2f965d3780dbb921b
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2025-10-23 23:11:15 +0900

    first commit

---
+++ src/main/java/com/example/buddyhouse/BuddyhouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.buddyhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddyhouseApplication.class, args);
+	}
+
+}
