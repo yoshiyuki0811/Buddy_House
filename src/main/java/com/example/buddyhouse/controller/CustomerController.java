3f7ef53d0cce398351429201b6381195dd684e4a
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2025-11-01 06:37:36 +0900

    復元

---
+++ src/main/java/com/example/buddyhouse/controller/CustomerController.java
@@ -0,0 +1,23 @@
+package com.example.buddyhouse.controller;
+
+import com.example.buddyhouse.dto.CustomerDto;
+import com.example.buddyhouse.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+@RequiredArgsConstructor
+public class CustomerController {
+private  final CustomerService customerService;
+
+@PostMapping
+  public ResponseEntity<CustomerDto> createCustomer(@RequestBody CustomerDto dto){
+  CustomerDto saved = customerService.createCustomer(dto);
+  return ResponseEntity.ok(saved);
+}
+}
