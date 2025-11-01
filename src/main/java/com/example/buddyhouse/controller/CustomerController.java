f8e4ca74226910085238794ed47afc4b1254ba0c
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2025-11-02 01:14:25 +0900

    controller復元

--- src/main/java/com/example/buddyhouse/controller/CustomerController.java
+++ src/main/java/com/example/buddyhouse/controller/CustomerController.java
@@ -1,9 +1,14 @@
 package com.example.buddyhouse.controller;
 
 import com.example.buddyhouse.dto.CustomerDto;
+import com.example.buddyhouse.dto.CustomersListDto;
 import com.example.buddyhouse.service.CustomerService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +19,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CustomerController {
 private  final CustomerService customerService;
-
+//
 @PostMapping
   public ResponseEntity<CustomerDto> createCustomer(@RequestBody CustomerDto dto){
   CustomerDto saved = customerService.createCustomer(dto);
   return ResponseEntity.ok(saved);
 }
+
+
+@GetMapping
+public ResponseEntity<List<CustomersListDto>> getCustomersList(){
+  List<CustomersListDto> customers =customerService.getCustomersList();
+  return ResponseEntity.ok(customers);
+  }
+  @GetMapping("/{id}")
+  public ResponseEntity<CustomerDto> getCustomersById(@PathVariable Long id){
+  CustomerDto customer = customerService.getCustomersById(id);
+  return ResponseEntity.ok(customer);
+  }
+@PatchMapping("/{id}/delete")
+public ResponseEntity<CustomerDto> deletedCustomersById(@PathVariable Long id) {
+  CustomerDto deleted = customerService.deleteCustomerById(id);
+  return ResponseEntity.ok(deleted);
 }
+  @PatchMapping("/{id}")
+  public ResponseEntity<CustomerDto> updateCustomersById(@PathVariable Long id,@RequestBody CustomerDto dto) {
+    CustomerDto update = customerService.updateCustomerById(id,dto);
+    return ResponseEntity.ok(update);
+  }
+}
+
+
