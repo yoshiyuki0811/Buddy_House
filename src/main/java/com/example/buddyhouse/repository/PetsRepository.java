03bce7f835a7952bd8dfff28bb6cdc9eea36747a
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2025-11-07 04:30:30 +0900

    特定の顧客のペットの一覧取得のリファクタリング

--- src/main/java/com/example/buddyhouse/repository/PetsRepository.java
+++ src/main/java/com/example/buddyhouse/repository/PetsRepository.java
@@ -1,23 +1,23 @@
 package com.example.buddyhouse.repository;
 
 
-import com.example.buddyhouse.dto.PetsListDto;
+
 import com.example.buddyhouse.entity.PetsEntity;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PetsRepository extends JpaRepository<PetsEntity,Long> {
-  @Query("SELECT new com.example.buddyhouse.dto.PetsListDto(p.id, p.customer.id, p.name, p.breed, p.weight )"+ "FROM PetsEntity p")
-  List<PetsListDto> findAllPetsList();
-  @Query("""
-    SELECT new com.example.buddyhouse.dto.PetsListDto(
-      p.id, p.customer.id, p.name, p.breed, p.weight
-    )
-    FROM PetsEntity p
-    WHERE p.customer.id = :customerId
-  """)
-  List<PetsListDto> findPetsByCustomerId(Long customerId);
+//  @Query("SELECT new com.example.buddyhouse.dto.PetsListDto(p.id, p.customer.id, p.name, p.breed, p.weight )"+ "FROM PetsEntity p")
+//  List<PetsListDto> findAllPetsList();
+//  @Query("""
+//    SELECT new com.example.buddyhouse.dto.PetsListDto(
+//      p.id, p.customer.id, p.name, p.breed, p.weight
+//    )
+//    FROM PetsEntity p
+//    WHERE p.customer.id = :customerId
+//  """)
+//  List<PetsListDto> findPetsByCustomerId(Long customerId);
+List<PetsEntity> findAllByCustomerIdAndDeletedFalse(Long customerId);
 }
