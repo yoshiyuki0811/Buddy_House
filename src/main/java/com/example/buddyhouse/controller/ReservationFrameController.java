44fb413104c5e97a535ff5f24f016505afda4658
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2025-11-16 08:33:46 +0900

    予約枠の日付毎の取得機能の実装

--- src/main/java/com/example/buddyhouse/controller/ReservationFrameController.java
+++ src/main/java/com/example/buddyhouse/controller/ReservationFrameController.java
@@ -3,8 +3,10 @@ package com.example.buddyhouse.controller;
 import com.example.buddyhouse.dto.ReservationFrameDto;
 import com.example.buddyhouse.dto.ReservationFrameListDto;
 import com.example.buddyhouse.service.ReservationFrameService;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,6 +32,13 @@ public class ReservationFrameController {
   @GetMapping
   public  ResponseEntity<List<ReservationFrameListDto>> getAllReservationFrame(){
     List<ReservationFrameListDto> dtoList = reservationFrameService.getReservationFrameList();
+    return ResponseEntity.ok(dtoList);
+  }
+
+  @GetMapping("/by-date")
+  public  ResponseEntity<List<ReservationFrameListDto>> getReservationFrameByDate( @RequestParam("dateTime")
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime dateTime){
+    List<ReservationFrameListDto> dtoList = reservationFrameService.getReservationFrameListByDate(dateTime);
     return ResponseEntity.ok(dtoList);
   }
 
--- src/main/java/com/example/buddyhouse/service/ReservationFrameService.java
+++ src/main/java/com/example/buddyhouse/service/ReservationFrameService.java
@@ -6,6 +6,7 @@ import com.example.buddyhouse.entity.ReservationFrameEntity;
 import com.example.buddyhouse.mapper.ReservationFrameMapper;
 import com.example.buddyhouse.repository.ReservationFrameRepository;
 import jakarta.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,15 @@ public class ReservationFrameService {
     List<ReservationFrameEntity> entity =reservationFrameRepository.findAll();
 
     return entity
+        .stream().map(reservationFrameMapper::toListDto)
+        .toList();
+
+  }
+  public List<ReservationFrameListDto> getReservationFrameListByDate(LocalDateTime date) {
+    LocalDateTime start =date.toLocalDate().atStartOfDay();
+    LocalDateTime end = date.toLocalDate().atTime(23,59,59,999999999);
+
+    return reservationFrameRepository.findByStartAtBetween(start,end)
         .stream().map(reservationFrameMapper::toListDto)
         .toList();
 
