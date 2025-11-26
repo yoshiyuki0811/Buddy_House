885609800d8eb326db5aedbfeeeedf0b1ad31045
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2025-11-27 06:26:26 +0900

    予約詳細取得機能の実装

--- src/main/java/com/example/buddyhouse/repository/ReservationRepository.java
+++ src/main/java/com/example/buddyhouse/repository/ReservationRepository.java
@@ -1,11 +1,14 @@
 package com.example.buddyhouse.repository;
 
+import com.example.buddyhouse.dto.ReservationListDto;
 import com.example.buddyhouse.entity.ReservationEntity;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ReservationRepository extends JpaRepository<ReservationEntity,Long> {
   //予約一覧用
@@ -15,6 +18,14 @@ public interface ReservationRepository extends JpaRepository<ReservationEntity,L
   //予約詳細取得用
   @EntityGraph(value = "Reservation.withDetails",type = EntityGraphType.LOAD)
   Optional<ReservationEntity> findById(Long id);
+
+  //日付別検索
+  @EntityGraph(value = "Reservation.withDetails",type = EntityGraphType.LOAD)
+  List<ReservationEntity> findByStartAtBetween(
+      LocalDateTime start, LocalDateTime end
+  );
+
+
 
 
 }
