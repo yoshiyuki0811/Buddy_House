25b7f73593f690546df15b6f6628e0809a8b47a7
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2025-12-11 03:38:55 +0900

    顧客が自分の顧客情報を取得機能を実装

--- src/main/java/com/example/buddyhouse/controller/customer/CustomerController.java
+++ src/main/java/com/example/buddyhouse/controller/customer/CustomerController.java
@@ -1,9 +1,14 @@
-package com.example.buddyhouse.controller;
+package com.example.buddyhouse.controller.customer;
 
+import com.example.buddyhouse.dto.customer.CustomerDetailDto;
 import com.example.buddyhouse.dto.customer.CustomerDto;
+import com.example.buddyhouse.security.CustomUserDetails;
+import com.example.buddyhouse.service.AuthService;
 import com.example.buddyhouse.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
   private final CustomerService customerService;
+  private final AuthService authService;
 
 
   @PostMapping
@@ -22,10 +28,11 @@ public class CustomerController {
     CustomerDto saved = customerService.createCustomer(dto);
     return ResponseEntity.ok(saved);
   }
-//  @GetMapping("/me")
-//  public CustomerDetailDto getCustomerMe(@AuthenticationPrincipal CustomUserDetails user){
-//    Long customerId = user.getcu
-//  }
+  @GetMapping("/me")
+  public CustomerDetailDto getCustomerMe(@AuthenticationPrincipal CustomUserDetails user){
+    Long customerId = authService.getLoggedInCustomerId(user.getUsername());
+    return customerService.getCustomersById(customerId);
+  }
 
 }
 
