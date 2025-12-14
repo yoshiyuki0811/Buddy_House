79aa226dfc9a75853819c1a2c948d18046b536ae
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2025-12-15 05:28:12 +0900

    Swaggerに認証機能の追加

---
+++ src/main/java/com/example/buddyhouse/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.example.buddyhouse.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(title = "Buddy House API", version = "1.0")
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+
+}
